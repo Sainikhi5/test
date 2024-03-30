6719e062b2b90ee2df47081a34276fc0ca92f054
Nikhila <ineedusainikhila@gmail.com>
2024-03-31 01:03:29 +0530

    updated sleepy feature in hungry.py

--- hungry.py
+++ hungry.py
@@ -1,6 +1,11 @@
 hungry=input("are you hungry")
+sleepy=input("are you tired?")
 if hungry=="yes":
     print("eat samosa")
 else:
     print("do your homework")
-    
+
+if sleepy=="yes":
+    print("take rest")
+else:
+    print("study")
